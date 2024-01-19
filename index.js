cc7d2185217e390a0e8217fde40190489e1872b4
harshit-2803 <harshitbramhe28@gmail.com>
2024-01-19 17:29:45 +0530

    add new

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function draw() {
     clearInterval(intervalId);
     ctx.fillStyle = 'red';
     ctx.font = '40px sans-serif';
-    ctx.fillText('Game over', 50, 150);
+    ctx.fillText('sorry gunna', 50, 150);
     return;
   }
 
